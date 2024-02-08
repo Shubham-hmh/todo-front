95c13619abf85ce20e4bebee7d9394ee77c56ecf
shubham-hmh <shubham_203054@saitm.org>
2024-02-08 13:54:10 +0530

    url change

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
-export const todo_url ="https://todo-back-zl36.onrender.com/api/todo";
+export const todo_url ="https://todo-back-zl36.onrender.com/api/todo/";
 
 // export const todo_url ="https://todolist-api-6olz.onrender.com/api/todo/";
 export const auth_url ="https://todolist-api-6olz.onrender.com/api/user/";
